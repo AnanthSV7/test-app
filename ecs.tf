3b8733967d3f8892efef6118399337b960d6ad92
AnanthSV7 <85213926+AnanthSV7@users.noreply.github.com>
2023-03-29 23:55:15 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,41 +1,32 @@
-resource "aws_ecs_task_definition" "my_task_definition" {
-  family                   = "my-task"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  network_mode             = "awsvpc"
+# Define the ECS task definition
+resource "aws_ecs_task_definition" "fargate_task_definition" {
+  family                   = "my-fargate-task" # Replace with your desired task family name
+  container_definitions    = jsonencode([{
+    name      = "test-app" # Replace with your desired container name
+    image     = "test-app"       # Replace with your desired Docker image
+    cpu       = 256           # Replace with your desired CPU units
+    memory    = 512           # Replace with your desired memory in MiB
+    portMappings = [{
+      containerPort = 3000     # Replace with your desired container port
+      hostPort      = 3000     # Replace with your desired host port
+    }]
+  }])
   requires_compatibilities = ["FARGATE"]
-
-  container_definitions = jsonencode([
-    {
-      name      = "demo-app"
-      image     = "demo-app:latest"
-      memory    = 512
-      cpu       = 256
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-    }
-  ])
+  network_mode             = "awsvpc"
+  memory                   = "1GB"
+  cpu                      = "256"
 }
 
-resource "aws_ecs_service" "ecs-cluster" {
-  name            = "ecs-cluster"
-  cluster         = aws_ecs_cluster.ecs-cluster.id
-  task_definition = aws_ecs_task_definition.my_task_definition.arn
+# Define the ECS Fargate service
+resource "aws_ecs_service" "fargate_service" {
+  name            = "my-fargate-service" # Replace with your desired service name
+  cluster         = aws_ecs_cluster.demo-app.id
+  task_definition = aws_ecs_task_definition.fargate_task_definition.arn
   desired_count   = 1
-
+  launch_type     = "FARGATE"
   network_configuration {
-    subnets = [
-      aws_subnet.ecs_subnet_1.id,
-      aws_subnet.ecs_subnet_2.id
-    ]
-
-    security_groups = [
-      aws_security_group.ecs_security_group.id
-    ]
-
-    assign_public_ip = "ENABLED"
+    subnets          = [aws_subnet.fargate_subnet.id]
+    security_groups  = [aws_security_group.fargate_security_group.id]
+    assign_public_ip = true
   }
 }
