92f15a40fad89423387fe303ff8291a98ecfe203
AnanthSV7 <85213926+AnanthSV7@users.noreply.github.com>
2023-03-29 23:57:12 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,10 +1,11 @@
-resource "aws_security_group" "ecs_security_group" {
-  name_prefix = "ecs-security-group"
+resource "aws_security_group" "fargate_security_group" {
+  name_prefix = "fargate-sg"
+  vpc_id      = aws_vpc.fargate_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
