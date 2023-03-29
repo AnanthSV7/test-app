308c8ec798972fb14c7afd73056a053dddaa593d
AnanthSV7 <85213926+AnanthSV7@users.noreply.github.com>
2023-03-29 23:58:51 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,8 @@
-resource "aws_vpc" "ecs_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "ecs-vpc"
-  }
+resource "aws_vpc" "fargate_vpc" {
+  cidr_block = "10.0.0.0/16" # Replace with your desired CIDR block
 }
 
-resource "aws_subnet" "ecs_subnet_1" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id     = aws_vpc.ecs_vpc.id
-
-  tags = {
-    Name = "ecs-subnet-1"
-  }
-}
-
-resource "aws_subnet" "ecs_subnet_2" {
-  cidr_block = "10.0.2.0/24"
-  vpc_id     = aws_vpc.ecs_vpc.id
-
-  tags = {
-    Name = "ecs-subnet-2"
-  }
+resource "aws_subnet" "fargate_subnet" {
+  vpc_id     = aws_vpc.fargate_vpc.id
+  cidr_block = "10.0.1.0/24" # Replace with your desired CIDR block
 }
